bb66a5eb9f044b55d758759ce15939fa3a1cf251
Robert Bedichek <robert@bedichek.org>
2025-05-31 20:26:57 -0700

    Replace hard coded user directory with $HOME

--- stop_solartracker.sh
+++ stop_solartracker.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-sudo launchctl bootout gui/$(id -u) /Users/robertbedichek/Library/LaunchAgents/com.bedichek.watch_gobble_solartracker.plist
-ps aux|egrep gobble_solartracker
+sudo launchctl bootout gui/$(id -u) $HOME/Library/LaunchAgents/com.bedichek.watch_gobble_solartracker.plist
+ps aux|egrep solartracker
